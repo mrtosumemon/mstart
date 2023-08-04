20532c377a3e4fc4d420b8ba6b3c4efcb23bdf6d
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-08-04 15:23:33 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mrtosumemon/GODFATHERMUSIC",
+    "https://github.com/mrtosumemon/mstart",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
